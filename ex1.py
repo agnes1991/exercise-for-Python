14697ec750976768555dfe3017e6ba0fb64247fa
agnes1991 <rui_huang@outlook.com>
2016-03-31 15:58:49 +0800

    ex1.py

---
+++ ex1.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+#打印
+print "Hello world!"
+print "Hello again!"
+print "I like typing this."
+print "This is fun."
+print 'Yeah!Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "I'm typing Python."
